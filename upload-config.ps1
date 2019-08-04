4eff2aff771a8d5319bf0dc97e571be4a99db1e5
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2019-08-04 11:57:49 +0100

    Simple script to upload a file to an app setting
    App settings are encrypted, so we can store the tokens there.

---
+++ upload-config.ps1
@@ -0,0 +1,13 @@
+param(
+    [string] $ResourceGroup,
+    [string] $AppName,
+    [string] $ConfigFile,
+    [string] $VariableName = "RCLONE_CONFIG"
+)
+
+$ErrorActionPreference = "Stop"
+
+$cfg = Get-Content $ConfigFile
+$cfgString = [system.String]::Join("\n", $cfg)
+
+az functionapp config appsettings set -g $ResourceGroup -n $AppName --settings "$VariableName=$CfgString"
