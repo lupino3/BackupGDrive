91ce26ed7e918dc8b007fbfe2442d0e5c7188c4e
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2021-05-09 15:46:51 +0200

    feat: improve manual backup script
    * make backups table-driven;
    * add basic error checking for rclone presence/config;
    * add cmdlet parameters.

--- tools/manual-backup.ps1
+++ tools/manual-backup.ps1
@@ -1,36 +1,69 @@
-# Script currently used for manual backups. Assumes that rclone is installed and configured.
+[CmdletBinding()]
+param(
+    [Parameter(HelpMessage="Set to true to back up photos, false will back up documents only.")]
+    [switch]$BackupPhotos = $false,
 
-# Docs to back up that live on GDrive.
-Write-Host "-----------------------------------------"
-Write-Host "Documents to back up from GDrive to Azure"
-Write-Host "-----------------------------------------"
-rclone sync 'GDrive:Important Documents' Azure:importantdocuments -P
+    [Parameter(HelpMessage="Path to the rclone binary (including the file name). E.g.: 'c:\rclone.exe'.")]
+    [ValidateNotNullOrEmpty()]
+    [string] $RclonePath = "$PSScriptRoot\rclone.exe",
 
-Write-Host "----------------------------------------------"
-Write-Host "Documents to back up from GDrive to Local Disk"
-Write-Host "----------------------------------------------"
-rclone sync 'GDrive:Important Documents' "d:\Important Documents" -P
+    # Rclone options.
+    [int]$RcloneCheckers = 5,
+    [int]$RcloneTransfers = 5
+)
+$ErrorActionPreference = "stop"
+$backups = @(
+    [pscustomobject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="Azure:importantdocuments"; Category="documents"},
+    [PSCustomObject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="d:\Important Documents"; Category="documents"},
+    # Copy from d:\Photos to cloud storage because there are more photos online than locally.
+    [PSCustomObject]@{Action="copy"; Source="d:\Photos"; Destination="GDrive:Photos"; Category="photos"},
+    [PSCustomObject]@{Action="sync"; Source="GDrive:Photos"; Destination="Azure:Photos"; Category="photos"}
+)
 
-# Photos: first local disk to GDrive (copy only, since GDrive has more photos).
-Write-Host "--------------------------------"
-Write-Host "Photos from local disk to GDrive"
-Write-Host "--------------------------------"
-rclone copy d:\Photos 'GDrive:Photos' -P 
+# Validation and setup.
+if (-not (Test-Path $RClonePath)) {
+    Write-Error "Could not find the RClone binary at $RClonePath"
+    exit
+}
 
-# Photos: then sync GDrive to Azure.
-Write-Host "---------------------------"
-Write-Host "Photos from GDrive to Azure"
-Write-Host "---------------------------"
-rclone sync 'GDrive:Photos' 'Azure:photos' -P --checkers=20 --transfers=20
+# TODO: get the necessary remotes from Source/Destination.
+$necessaryRemotes = @("Azure:", "GDrive:")
+$remotes = Invoke-Expression "$RClonePath listremotes"
+$ok = $true
+$necessaryRemotes | % {
+    if (-not ($remotes -contains $_)) {
+        Write-Error "Could not find remote $_" -EA:Continue
+        $ok = $false
+    }
+}
+if (-not $ok) {
+    exit
+}
 
-# Old HD Backup local -> GDrive
-Write-Host "-----------------------------------"
-Write-Host "HD Backup from local disk to GDrive"
-Write-Host "-----------------------------------"
-rclone sync "d:\HD Backup" 'GDrive:HD Backup' -P --checkers=20 --transfers=20
+# Future categories support might be more advanced, for now let's just allow
+# to skip backing up photos.
+$categories = @("documents")
+if ($BackupPhotos) {
+    $categories = @("documents";"photos")
+}
 
-# Old HD Backup GDrive -> Azure
-Write-Host "------------------------------"
-Write-Host "HD Backup from GDrive to Azure"
-Write-Host "------------------------------"
-rclone sync 'GDrive:HD Backup' Azure:hdbackup -P --checkers=20 --transfers=20
+# Script currently used for manual backups. Assumes that rclone is available in the directory of the script and configured.
+$rcloneParams = "-P --checkers=$RcloneCheckers --transfers=$RcloneTransfers"
+
+$backups | ForEach-Object {
+    if (-not ($categories -icontains $_.Category)) {
+        Write-Host "Skipping $($_.Source) → $($_.Destination) as it's in category $($_.Category)"
+        return
+    }
+    $msg = "$($_.Action) $($_.Category): $($_.Source) → $($_.Destination)"
+    $spacer = "-" * ($msg.Length)
+
+    Write-Host "$spacer`n$msg`n$spacer"
+
+    $cmd = "$RClonePath $($_.Action) '$($_.Source)' '$($_.Destination)' $rcloneParams"
+    Write-Host $cmd
+    Invoke-Expression $cmd
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "There was an error (code $LASTEXITCODE) when doing the transfer. Check the rclone log for more info."
+    }
+}
