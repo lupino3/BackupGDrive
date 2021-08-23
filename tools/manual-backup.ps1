d8a491b8a0884b4eba8eced55460977820b91e42
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2021-08-23 17:21:02 +0200

    feat: move from categories to list of jobs
    No need for "categories", which become problematic when having 1:N
    mappings.
    
    Also add ShouldProcess support.

--- tools/manual-backup.ps1
+++ tools/manual-backup.ps1
@@ -1,4 +1,4 @@
-[CmdletBinding()]
+[CmdletBinding(SupportsShouldProcess)]
 param(
     [Parameter(HelpMessage="Set to true to back up photos, false will back up documents only.")]
     [switch]$BackupPhotos = $false,
@@ -15,20 +15,35 @@ param(
     [switch]
     $ShouldDownloadRclone = $false,
 
+    [Parameter(HelpMessage="List of backup jobs to run. Possible values: syncdocs, localcopydocs, uploadphotos, syncphotos")]
+    [string[]]
+    $BackupsToRun = @("syncdocs"),
+
     # Rclone options.
     [int]$RcloneCheckers = 5,
     [int]$RcloneTransfers = 5
 )
 $ErrorActionPreference = "stop"
-$backups = @(
-    [pscustomobject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="Azure:importantdocuments"; Category="documents"},
-    [PSCustomObject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="d:\Important Documents"; Category="documents"},
-    # Copy from d:\Photos to cloud storage because there are more photos online than locally.
-    [PSCustomObject]@{Action="copy"; Source="d:\Photos"; Destination="GDrive:Photos"; Category="photos"},
-    [PSCustomObject]@{Action="sync"; Source="GDrive:Photos"; Destination="Azure:Photos"; Category="photos"}
-)
 
-# Validation and setup.
+# TODO: move to config file.
+$backups = @{
+    "syncdocs" = [pscustomobject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="Azure:importantdocuments"; Categories=@("documents","cloud")};
+    "localcopydocs" = [PSCustomObject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="d:\Important Documents"; Category=@("documents","tolocal")};
+    # Copy from d:\Photos to cloud storage because there are more photos online than locally.
+    "uploadphotos" = [PSCustomObject]@{Action="copy"; Source="d:\Photos"; Destination="GDrive:Photos"; Categories=@("photos","fromlocal")};
+    "syncphotos" = [PSCustomObject]@{Action="sync"; Source="GDrive:Photos"; Destination="Azure:Photos"; Categories=@("photos","cloud")};
+}
+# Validate command line options.
+$invalidBackups = $BackupsToRun | ? {-not $backups.Contains($_)}
+if ($invalidBackups.Count -gt 0) {
+    Write-Error "Invalid backup names: $InvalidBackups." 
+    exit
+}
+
+$toRun = $backups.keys | ? {$BackupsToRun -contains $_}
+Write-Debug "Will run $toRun backups."
+
+# Validation and setup
 if (-not (Test-Path $RCloneConfig)) {
     Write-Error "Could not find the RClone config at $RCloneConfig"
     exit
@@ -63,26 +78,20 @@ if (-not $ok) {
     exit
 }
 
-# Future categories support might be more advanced, for now let's just allow
-# to skip backing up photos.
-$categories = @("documents")
-if ($BackupPhotos) {
-    $categories = @("documents";"photos")
-}
+foreach ($backupName in $toRun) {
+    $backup = $backups[$backupName]
+    Write-Debug "Running $backupName"
 
-$backups | ForEach-Object {
-    if (-not ($categories -icontains $_.Category)) {
-        Write-Host "Skipping $($_.Source) → $($_.Destination) as it's in category $($_.Category)"
-        return
-    }
-    $msg = "$($_.Action) $($_.Category): $($_.Source) → $($_.Destination)"
+    $msg = "[$backupName] $($backup.Action) $($backup.Category): $($backup.Source) → $($backup.Destination)"
     $spacer = "-" * ($msg.Length)
 
     Write-Host "$spacer`n$msg`n$spacer"
 
-    $cmd = "$RClonePath $($_.Action) '$($_.Source)' '$($_.Destination)' $rcloneParams"
+    $cmd = "$RClonePath $($backup.Action) '$($backup.Source)' '$($backup.Destination)' $rcloneParams"
     Write-Host $cmd
-    Invoke-Expression $cmd
+    if ($PSCmdlet.ShouldProcess($cmd)) {
+        Invoke-Expression $cmd
+    }
     if ($LASTEXITCODE -ne 0) {
         Write-Error "There was an error (code $LASTEXITCODE) when doing the transfer. Check the rclone log for more info."
     }
