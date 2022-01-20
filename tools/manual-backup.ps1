75c851f0acec546e0a68487e0378c56d065f141f
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2022-01-20 16:11:33 +0100

    feat: read config from an environment variable

--- tools/manual-backup.ps1
+++ tools/manual-backup.ps1
@@ -4,9 +4,15 @@ param(
     [ValidateNotNullOrEmpty()]
     [string] $RclonePath = "$PSScriptRoot\rclone.exe",
 
-    [Parameter(HelpMessage="Paths where to search the rclone config file (rclone.conf)")]
+    [Parameter(HelpMessage="Paths where to search the rclone config file (rclone.conf)",
+               ParameterSetName = "Path")]
     [ValidateNotNullOrEmpty()]
     [string[]] $RCloneConfigSearchPath = @(".", "$env:userprofile\.config\rclone", "$env:APPDATA\rclone"),
+
+    [Parameter(HelpMessage="Environment variable containing a Base64-encoded RClone configuration.",
+               ParameterSetName="Env")]
+    [ValidateNotNullOrEmpty()]
+    [string] $RCloneConfigEnvVariableName,
 
     [Parameter(HelpMessage="Set to true if rclone should be downloaded if missing")]
     [switch] $ShouldDownloadRclone,
@@ -18,86 +24,110 @@ param(
     [Parameter()] [int]$RcloneCheckers = 5,
     [Parameter()] [int]$RcloneTransfers = 5
 )
-$ErrorActionPreference = "stop"
+begin {
+    $ErrorActionPreference = "stop"
 
-# TODO: move to config file.
-$backups = @{
-    "syncdocs" = [pscustomobject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="Azure:importantdocuments"};
-    "localcopydocs" = [PSCustomObject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="f:\Important Documents"};
-    "uploadphotos" = [PSCustomObject]@{Action="copy"; Source="e:\Photos"; Destination="GDrive:Photos"}; # Copy from d:\Photos to cloud storage because there are more photos online than locally.
-    "syncphotos" = [PSCustomObject]@{Action="sync"; Source="GDrive:Photos"; Destination="Azure:Photos"};
-}
-# Validate command line options.
-$invalidBackups = $BackupsToRun | ? {-not $backups.Contains($_)}
-if ($invalidBackups.Count -gt 0) {
-    Write-Error "Invalid backup names: $InvalidBackups." 
-    exit
-}
-
-$toRun = $backups.keys | ? {$BackupsToRun -contains $_}
-Write-Debug "Will run $toRun backups."
-
-$RcloneConfig = ""
-$found = $false
-foreach($path in $RCloneConfigSearchPath) {
-    $RcloneConfig = "$path\rclone.conf"
-    Write-Debug "Trying $RcloneConfig"
-    if (Test-Path $RcloneConfig) {
-        $found = $true
-        break
+    # TODO: move to config file.
+    $backups = @{
+        "syncdocs" = [pscustomobject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="Azure:importantdocuments"};
+        "localcopydocs" = [PSCustomObject]@{Action="sync"; Source="GDrive:Important Documents"; Destination="f:\Important Documents"};
+        "uploadphotos" = [PSCustomObject]@{Action="copy"; Source="e:\Photos"; Destination="GDrive:Photos"}; # Copy from d:\Photos to cloud storage because there are more photos online than locally.
+        "syncphotos" = [PSCustomObject]@{Action="sync"; Source="GDrive:Photos"; Destination="Azure:Photos"};
     }
-}
-
-# Validation and setup
-if (-not $found) {
-    Write-Error "Could not find the RClone config in $RCloneConfigSearchpath"
-    exit
-}
-if (-not (Test-Path $RClonePath)) {
-    if (-not $ShouldDownloadRclone) {
-        Write-Error "Could not find the RClone binary at $RClonePath"
+    # Validate command line options.
+    $invalidBackups = $BackupsToRun | ? {-not $backups.Contains($_)}
+    if ($invalidBackups.Count -gt 0) {
+        Write-Error "Invalid backup names: $InvalidBackups." 
         exit
     }
-    $RcloneUri = "https://downloads.rclone.org/rclone-current-windows-amd64.zip" 
-    Write-Output "Could not find the RClone binary at $RClonePath. Downloading it as requested, from $RcloneUri"
-    Invoke-WebRequest -Uri $RcloneUri -OutFile $env:TMP\rclone.zip
-    Expand-Archive -Force $env:TMP\rclone.zip -DestinationPath $env:TMP
-    $RclonePath = Get-ChildItem -Path $env:TMP\rclone-v*-windows-amd64\rclone.exe
-    Write-Debug "Overriding Rclone path to $RclonePath"
-}
 
-# Common rclone parameters.
-$rcloneParams = "-P --checkers=$RcloneCheckers --transfers=$RcloneTransfers --config=$RcloneConfig"
+    $toRun = $backups.keys | ? {$BackupsToRun -contains $_}
+    Write-Debug "Will run $toRun backups."
 
-# TODO: get the necessary remotes from Source/Destination.
-$necessaryRemotes = @("Azure:", "GDrive:")
-$remotes = Invoke-Expression "$RClonePath listremotes $rcloneParams"
-$ok = $true
-$necessaryRemotes | % {
-    if (-not ($remotes -contains $_)) {
-        Write-Error "Could not find remote $_" -EA:Continue
-        $ok = $false
+    $RcloneConfigPath = ""
+    if ($RCloneConfigEnvVariableName) {
+        $data = [System.Environment]::GetEnvironmentVariable($RCloneConfigEnvVariableName)
+        if (-not $data) {
+            Write-Error "The environment variable $RcloneConfigEnvVariableName is empty."
+            exit
+        }
+        $tempFile = New-TemporaryFile
+        Write-Debug "Created temporary file $tempfile."
+        $decodedConfig = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($data))
+        Write-Debug "Decoded config."
+        $decodedConfig | Out-File -FilePath $tempFile 
+        $RcloneConfigPath = $tempFile
+    } else {
+        $found = $false
+        foreach($path in $RCloneConfigSearchPath) {
+            $RcloneConfigPath = "$path\rclone.conf"
+            Write-Debug "Trying $RcloneConfigPath"
+            if (Test-Path $RcloneConfigPath) {
+                $found = $true
+                break
+            }
+        }
+
+        # Validation and setup
+        if (-not $found) {
+            Write-Error "Could not find the RClone config in $RCloneConfigSearchpath"
+            exit
+        }
+    }
+    Write-Debug "Using RClone config at $RcloneConfigPath"
+    if (-not (Test-Path $RClonePath)) {
+        if (-not $ShouldDownloadRclone) {
+            Write-Error "Could not find the RClone binary at $RClonePath"
+            exit
+        }
+        $RcloneUri = "https://downloads.rclone.org/rclone-current-windows-amd64.zip" 
+        Write-Output "Could not find the RClone binary at $RClonePath. Downloading it as requested, from $RcloneUri"
+        Invoke-WebRequest -Uri $RcloneUri -OutFile $env:TMP\rclone.zip
+        Expand-Archive -Force $env:TMP\rclone.zip -DestinationPath $env:TMP
+        $RclonePath = Get-ChildItem -Path $env:TMP\rclone-v*-windows-amd64\rclone.exe
+        Write-Debug "Overriding Rclone path to $RclonePath"
+    }
+
+    # Common rclone parameters.
+    $rcloneParams = "-P --checkers=$RcloneCheckers --transfers=$RcloneTransfers --config=$RcloneConfigPath"
+
+    # TODO: get the necessary remotes from Source/Destination.
+    $necessaryRemotes = @("Azure:", "GDrive:")
+    $remotes = Invoke-Expression "$RClonePath listremotes $rcloneParams"
+    $ok = $true
+    $necessaryRemotes | % {
+        if (-not ($remotes -contains $_)) {
+            Write-Error "Could not find remote $_" -EA:Continue
+            $ok = $false
+        }
+    }
+    if (-not $ok) {
+        exit
     }
 }
-if (-not $ok) {
-    exit
-}
+process {
+    foreach ($backupName in $toRun) {
+        $backup = $backups[$backupName]
+        Write-Debug "Running $backupName"
 
-foreach ($backupName in $toRun) {
-    $backup = $backups[$backupName]
-    Write-Debug "Running $backupName"
+        $msg = "[$backupName] $($backup.Action) $($backup.Category): $($backup.Source) → $($backup.Destination)"
+        $spacer = "-" * ($msg.Length)
 
-    $msg = "[$backupName] $($backup.Action) $($backup.Category): $($backup.Source) → $($backup.Destination)"
-    $spacer = "-" * ($msg.Length)
+        Write-Host "$spacer`n$msg`n$spacer"
 
-    Write-Host "$spacer`n$msg`n$spacer"
-
-    $cmd = "$RClonePath $($backup.Action) '$($backup.Source)' '$($backup.Destination)' $rcloneParams"
-    Write-Host $cmd
-    if ($PSCmdlet.ShouldProcess($cmd)) {
-        Invoke-Expression $cmd
+        $cmd = "$RClonePath $($backup.Action) '$($backup.Source)' '$($backup.Destination)' $rcloneParams"
+        Write-Host $cmd
+        if ($PSCmdlet.ShouldProcess($cmd)) {
+            Invoke-Expression $cmd
+        }
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error "There was an error (code $LASTEXITCODE) when doing the transfer. Check the rclone log for more info."
+        }
     }
-    if ($LASTEXITCODE -ne 0) {
-        Write-Error "There was an error (code $LASTEXITCODE) when doing the transfer. Check the rclone log for more info."
+}
+end {
+    if ($tempFile) {
+        Write-Debug "Removing $tempile"
+        rm $tempFile
     }
 }
