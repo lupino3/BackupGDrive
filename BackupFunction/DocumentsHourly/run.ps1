086c1243dcd80686b39df50a4d92da3ad113eea1
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2022-01-26 12:52:33 +0100

    refactor: rename Hourly in DocumentsHourly

--- BackupFunction/DocumentsHourly/run.ps1
+++ BackupFunction/DocumentsHourly/run.ps1
@@ -2,6 +2,5 @@ param($Timer)
 
 Import-Module rclone.psm1
 
-Write-Host "Starting Invoke-Rclone."
-
+Write-Host "Starting hourly documents backup."
 Invoke-Rclone -ShouldDownloadRclone -BackupsToRun syncdocs -RcloneConfigEnvVariableName RCLONE_CONFIG_CONTENTS -Interactive:$false
