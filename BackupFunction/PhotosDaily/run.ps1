5e203edc802768ffdd8410e6e70abe5a79bf0d2f
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2022-12-31 15:07:05 +0100

    fix: increase parallelism and timeout for daily photos backup

--- BackupFunction/PhotosDaily/run.ps1
+++ BackupFunction/PhotosDaily/run.ps1
@@ -3,4 +3,4 @@ param($Timer)
 Import-Module rclone.psm1
 
 Write-Host "Starting daily photos backup."
-Invoke-Rclone -ShouldDownloadRclone -BackupsToRun syncphotos -RcloneConfigEnvVariableName RCLONE_CONFIG_CONTENTS -Interactive:$false
+Invoke-Rclone -ShouldDownloadRclone -BackupsToRun syncphotos -RcloneConfigEnvVariableName RCLONE_CONFIG_CONTENTS -Interactive:$false -RCloneCheckers 20 -RCloneTransfers 20
